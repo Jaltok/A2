28e3bdf43c2bbbb5faf81d94b09f9e4fb7de82a8
Jaltok <lund.jeff@gmail.com>
2018-01-23 15:23:52 -0800

    first commit

---
+++ fp_parse.c
@@ -0,0 +1,14 @@
+/*********************************************
+ *  Jeff Lund
+ *  Systems A2: Floating point parser
+ *  *****************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char* argv[]) {
+    char* frac_bits = argv[1];
+    char* exp_bits = argv[2];
+    char* hex_to_convert = argv[3];
+}
