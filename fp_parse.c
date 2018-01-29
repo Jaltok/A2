5e581f2b9c6421e984845ccdd695c9ecaa215744
Jaltok <lund.jeff@gmail.com>
2018-01-29 15:38:00 -0800

    Added in assert to check hex argv is actually hex

--- fp_parse.c
+++ fp_parse.c
@@ -1,33 +1,46 @@
 /*********************************************
  *  Jeff Lund
  *  Systems A2: Floating point parser
- *  *****************************************/
-// notes: bias = 2^(k-1)-1 k is exp bits
-
+ ********************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <assert.h>
+#include <ctype.h>
 
-double isNegative(short unsigned int frac_bits, short unsigned int exp_bits, unsigned int hex_to_convert) {
+
+double isNegative(unsigned int frac_bits, unsigned int exp_bits, unsigned int hex_to_convert) {
     if((hex_to_convert>>(frac_bits+exp_bits))&1)
         return -1.0;
     else
         return 1.0;
 }
+double normalized(int frac_bits, int exp_bits, unsigned int hex_to_convert) {
+    
+}
+
+double denormalized(int frac_bits, int exp_bits, unsigned int hex_to_convert) {
+
+}
 
 int main(int argc, char* argv[]) {
-    short unsigned int frac_bits, exp_bits;// , isolated_exp, isolated_frac, isolated_sign;
+    int frac_bits, exp_bits, frac, exp;
     unsigned int hex_to_convert;
-    short unsigned int frac = 0; //isolated frac, n
-    short unsigned int exp = 0; //isolated exp, k
     int bias;
     double sign, M, V, E;
-	assert(argv[1] && argv[2] && argv[3]);
-    sscanf(argv[1], "%hu", &frac_bits);
-    sscanf(argv[2], "%hu", &exp_bits);
-    sscanf(argv[3], "%x", &hex_to_convert);
+    int len;
+
+    assert(argv[1] && argv[2] && argv[3]);
+    len = strlen(argv[3]);
+    for(int i = 0; i < len; i++)
+        assert(isxdigit(argv[3][i]));
+
+    sscanf(argv[1], "%8d", &frac_bits);
+    sscanf(argv[2], "%8d", &exp_bits);
+    sscanf(argv[3], "%8x", &hex_to_convert);
+
     if(frac_bits > 10 || frac_bits < 2) {
         printf("Illegal number of fraction bits(%hu). Should be between 2 and 10.\n", frac_bits);
         return 0;
