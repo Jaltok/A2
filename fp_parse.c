f323f8b26564bdc7a32b4e85f0669aed32c245ec
Jaltok <lund.jeff@gmail.com>
2018-01-29 16:51:06 -0800

    Cleaned up code

--- fp_parse.c
+++ fp_parse.c
@@ -1,7 +1,10 @@
-/*********************************************
+/*********************************************************************
  *  Jeff Lund
  *  Systems A2: Floating point parser
- ********************************************/
+ *  Takes in three command line arguments:
+ *  # of fraction bits, # of exponent bits, hex number to be converted
+ *  Prints out floating point representation
+ ***********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,50 +13,33 @@
 #include <assert.h>
 #include <ctype.h>
 
-
-double isNegative(unsigned int frac_bits, unsigned int exp_bits, unsigned int hex_to_convert) {
-    if((hex_to_convert>>(frac_bits+exp_bits))&1)
-        return -1.0;
-    else
-        return 1.0;
-}
-double normalized(int frac_bits, int exp_bits, unsigned int hex_to_convert) {
-    
-}
-
-double denormalized(int frac_bits, int exp_bits, unsigned int hex_to_convert) {
-
-}
-
 int main(int argc, char* argv[]) {
     int frac_bits, exp_bits, frac, exp;
     unsigned int hex_to_convert;
-    int bias;
-    double sign, M, V, E;
-    int len;
+    int bias, sign;
+    double M, E, V; // Matissa, Exponent, Value
 
     assert(argv[1] && argv[2] && argv[3]);
-    len = strlen(argv[3]);
-    for(int i = 0; i < len; i++)
+    for(int i = 0; i < strlen(argv[3]); i++)
         assert(isxdigit(argv[3][i]));
 
-    sscanf(argv[1], "%8d", &frac_bits);
-    sscanf(argv[2], "%8d", &exp_bits);
+    sscanf(argv[1], "%3d", &frac_bits);
+    sscanf(argv[2], "%3d", &exp_bits);
     sscanf(argv[3], "%8x", &hex_to_convert);
 
     if(frac_bits > 10 || frac_bits < 2) {
-        printf("Illegal number of fraction bits(%hu). Should be between 2 and 10.\n", frac_bits);
+        printf("Illegal number of fraction bits(%d). Should be between 2 and 10.\n", frac_bits);
         return 0;
     }
     if(exp_bits > 5 || exp_bits < 3) {
-        printf("Illegal number of exponent bits(%hu). Should be between 3 and 5.\n", exp_bits);
+        printf("Illegal number of exponent bits(%d). Should be between 3 and 5.\n", exp_bits);
         return 0;
     }
     
-    bias = pow(2, (exp_bits-1))-1; 
-    sign = isNegative(frac_bits, exp_bits, hex_to_convert);
-    frac = hex_to_convert&((1<<frac_bits)-1);
-    exp = (hex_to_convert>>frac_bits)&((1<<exp_bits)-1);
+    bias = pow(2, (exp_bits-1))-1; //calculates bias
+    sign = pow(-1, ((hex_to_convert>>(frac_bits+exp_bits))&1)); // calculates sign value  
+    frac = hex_to_convert&((1<<frac_bits)-1); //isolates fraction bits 
+    exp = (hex_to_convert>>frac_bits)&((1<<exp_bits)-1); //isolates exponent bits
     
     if(exp == 0) { //denormalized
         E = 1.0 - bias;
@@ -65,13 +51,13 @@ int main(int argc, char* argv[]) {
         printf("%f\n", V);
     }	
 	else if(exp == pow(2,exp_bits)-1) { //inf or NaN
-		if(frac == 0) { //infinite
-			if((hex_to_convert>>(frac_bits+exp_bits))&1) //neg inf
+		if(frac == 0) { 
+			if(sign == 1)
+                printf("+inf\n");
+			else 
 				printf("-inf\n");
-			else //pos inf
-				printf("+inf\n");
 		}
-		else //NaN
+		else 
 			printf("NaN\n");
 	}
 	else { //Normalized
